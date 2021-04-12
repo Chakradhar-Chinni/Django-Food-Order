d14013f0736693329d20d3ad8c678ca4a5de6b61
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-12 23:45:12 +0530

    Update models.py

--- foodorder/menu/models.py
+++ foodorder/menu/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class PickBurger(models.Model):
             CustomerPIN= models.CharField(max_length=10,blank=False)
-         ('Cheese Burger', 'Veg Burger', 'Chilli Burger')
+        
         
             Quantity = models.IntegerField()
             class Meta:
