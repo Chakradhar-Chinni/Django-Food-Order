cad40ed4b3e0520865c3ff8ef04e0093c491d25f
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-15 23:44:53 +0530

    Update views.py

--- foodorder/menu/views.py
+++ foodorder/menu/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
    # Create your views here.  
-
+  
 def mainpage(request):
      return render(request,'welcome.html')
     #return HttpResponse("Main Page static data")
