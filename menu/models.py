f2faff18db76bb04ee15bee6e01d960de7c1f3f1
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-28 22:38:24 +0530

    Update models.py

--- menu/models.py
+++ menu/models.py
@@ -8,3 +8,4 @@ class PickBurger(models.Model):
             Message = models.CharField(max_length=1000,blank=True)  #True -> Optional Field'
             class Meta:
                 db_table = "burger_logs" #currently all orders are stored in this table only
+            class Meta:
