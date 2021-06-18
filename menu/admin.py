2709ff3646f68b3cec7a5d28f962497cc6092444
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-18 23:42:53 +0530

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import PickBurger  
      
 # Register your models here.          
-admin.site.register(PickBurger)        
+admin.site.register(PickBurger)         
  
  
